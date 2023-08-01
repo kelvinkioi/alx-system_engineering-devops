9034513336a5a9e861883d8ea0c0daa7a232a3d3
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-08-01 02:22:18 -0700

    regexp that searches for words starting with h and ends with n and have a particular set of characters

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,4 +1,3 @@
 #!/usr/bin/env ruby
-#Regexp that searches for a particuar words starting
-#with h and ends with and have a particuar pattern of characters
+#Regexp that searches for a particuar words
 puts ARGV[0].scan(/h[b|t]{2,5}n/).join
