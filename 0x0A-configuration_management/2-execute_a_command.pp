2f28f03f894ddb0cb998bfa34b03824d5dffc981
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-09-22 02:36:05 -0700

    removed trailing whitespace in line5

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #Using Puppet, create a manifest that kills a process named killmenow
 
 exec { 'kills a process':
-  command => 'pkill "killmenow"',
-  path    => ['/usr/bin', '/usr/sbin',], 
+  command => 'pkill -f "killmenow"',
+  path    => ['/usr/bin', '/usr/sbin',],
 }
