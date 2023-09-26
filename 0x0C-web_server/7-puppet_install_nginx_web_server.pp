1bcf9760fb6ea08bdb765c3cb1a4b3df3a03b9bf
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-09-26 08:15:08 -0700

    fixed puppet-lint errors

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,24 @@
 # installing and configure an Nginx server using Puppet instead of Bash
 
 exec { 'system update using apt':
-        command => '/usr/bin/apt-get update',
+  command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update system']
+  ensure  => 'installed',
+  require => Exec['update system']
 }
 
 file {'/var/www/html/index.html':
-	content => 'Hello World!'
+  content => 'Hello World!',
 }
 
 exec {'redirect_me':
-	command => sudo sed -i '/server_name _;/a rewrite ^/redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default
-	provider => 'shell'
+  command  => sudo sed -i '/server_name _;/a rewrite ^/redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default
+  provider => 'shell',
 }
 
 service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+  ensure  => running,
+  require => Package['nginx']
 }
