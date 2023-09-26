b82b8a7276a7e0a0944825b1ba77ab0be83a8eba
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-09-26 08:30:00 -0700

    overhaul changes to my puppet file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,46 @@
 # installing and configure an Nginx server using Puppet instead of Bash
 
-exec { 'system update using apt':
-  command => '/usr/bin/apt-get update',
+# Updating packages using apt
+exec { 'apt-update':
+  command => 'apt-get -y update',
+  path    => ['/usr/bin', '/bin'],
 }
 
+# Installing Nginx
 package { 'nginx':
   ensure  => 'installed',
-  require => Exec['update system']
+  require => Exec['apt-update'],
 }
 
-file {'/var/www/html/index.html':
-  content => 'Hello World!',
+# Nginx HTTP traffic(Port 80)
+exec { 'allow-nginx-http':
+  command => 'ufw allow "Nginx HTTP"',
+  path    => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+  unless  => 'ufw status | grep "Nginx HTTP"',
+  require => Package['nginx'],
 }
 
-exec {'redirect_me':
-  command  => 'sudo sed -i '/server_name _;/a rewrite ^/redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default',
-  provider => 'shell',
+# Creating index file with "Hello World"
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => 'file',
+  content => 'Hello World',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  require => Package['nginx'],
 }
 
-service {'nginx':
-  ensure  => running,
-  require => Package['nginx']
+# Nginx rewrite rule
+exec { 'nginx-rewrite-rule':
+  command => 'sed -i "/server_name _;/a rewrite ^/redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  path    => ['/usr/bin', '/bin'],
+  require => Package['nginx'],
 }
+
+# Restarting Nginx service
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => [Exec['add-nginx-rewrite-rule'], Package['nginx']],
+}
+
