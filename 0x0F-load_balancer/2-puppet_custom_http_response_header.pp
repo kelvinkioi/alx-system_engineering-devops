87de175389cfb8bd2645f4317ae053e65a5aaebf
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-10-03 14:15:58 -0700

    automate the task of creating a custom HTTP header response, but with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,22 @@
+# automate the task of creating a custom HTTP header response, but with Puppet
+package {'nginx':
+  ensure => 'present',
+}
+
+exec { 'update and install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+}
+
+exec { 'sudo sed -i "/listen 80 default_server/a add_header X-Served-By $hostname;" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec { 'Hello World':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec { 'sudo service nginx restart':
+  provider => shell,
+}
